68d6180e96160c726d1fd8b34f44b389d00ba9d4
InvisibleUp <invisibleup@outlook.com>
2020-12-12 18:45:05 -0500

    MAP: It kind of does an infinite scroll now...

--- map.js
+++ map.js
@@ -4,12 +4,22 @@ let Y_POS = 128;
 let X_TILESIZE = 128;
 let Y_TILESIZE = 64;
 let TIME_START = 0;
+let TIME_LAST = 0;
 
 const COLORS = ['red', 'blue', 'cyan', 'green', 'purple', 'magenta', 'yellow', 'orange', 'teal', 'pink'];
+
+const NUM_BG = 1;
+let CANVAS_BG = [];
+let CTX_BG = [];
 
 // https://stackoverflow.com/a/24137301
 Array.prototype.random = function () {
 	return this[Math.floor((Math.random()*this.length))];
+}
+
+// https://stackoverflow.com/a/17323608
+function mod(n, m) {
+	return ((n % m) + m) % m;
 }
 
 // get an array of tiles to be rendered onto the screen
@@ -19,23 +29,23 @@ function Tiles_MakeRandom(w, h)
 	for(y = 0; y < h; y += 1) {
 		let row = [];
 		for (x = 0; x < w; x += 1) {
-			row.push(COLORS.random());
+			let color = COLORS.random();
+			row.push(color);
 		}
 		map.push(row);
 	}
-	console.log(map);
 	return map;
 }
 
 // just a generic grid-like thing for now
-function Render_Step_BG1(ctx, w, h)
+function Render_Step_BG(ctx, w, h)
 {
 	let x = 0;
-	let y = h / 2;
+	let y = (h / 2) - (Y_TILESIZE / 2);
 	let xi = 0;
 	let yi = 0;
 
-	const tiles = Tiles_MakeRandom(6, 6);
+	const tiles = Tiles_MakeRandom(8, 8);
 	for (const row of tiles) {
 		for (const tile of row) {
 			ctx.fillStyle = tile;
@@ -63,39 +73,81 @@ function Render_Step_BG1(ctx, w, h)
 // thing to do every frame
 function Render_Step(timestamp)
 {
+	// timestamp
+	let time = timestamp - TIME_START;
+
 	// primary canvas
 	const canvas = document.getElementById('mapcanvas');
 	if (!canvas.getContext) { return; }
 	const ctx = canvas.getContext('2d', {alpha: false});
-	// bg1 canvas
-	const canvas_bg1 = document.getElementById('bg1');
-	if (!canvas_bg1.getContext) { return; }
-	const ctx_bg1 = canvas_bg1.getContext('2d');
-
-	// timestamp
-	let time = timestamp - TIME_START;
 
 	// temporary motion thing
-	X_POS = Math.cos(time / 1000) * 64;
-	Y_POS = Math.sin(time / 1000) * 64;
+	X_POS = Math.cos(time / 2000) * 1024 - 512;
+	Y_POS = Math.sin(time / 500) * 128;
 
 	// blit background layer to composite canvas
-	const bg1 = ctx_bg1.getImageData(X_POS, Y_POS, 512, 512);
-	ctx.putImageData(bg1, 0, 0)
+	ctx.globalCompositeOperation = 'source-over';
+	ctx.clearRect(0, 0, 512, 512); // temporary!
 
+	let x_pos = Math.floor(mod(X_POS, CANVAS_BG[0].width));
+
+	const x_off = [
+		(x_pos),                              // far-top
+		(x_pos + (CANVAS_BG[0].width) / 2),   // top-left
+		(x_pos - (CANVAS_BG[0].width) / 2),   // top-right
+		(x_pos + CANVAS_BG[0].width),         // far-left
+		(x_pos),                              // center
+		(x_pos - CANVAS_BG[0].width),         // far-right
+		(x_pos + (CANVAS_BG[0].width) / 2),   // bottom-left
+		(x_pos - (CANVAS_BG[0].width) / 2),   // bottom-right
+		(x_pos)                               // far-bottom
+	];
+	const y_off = [
+		(Y_POS - CANVAS_BG[0].height),         // far-top
+		(Y_POS + (CANVAS_BG[0].height) / 2),   // top-left
+		(Y_POS + (CANVAS_BG[0].height) / 2),   // top-right
+		(Y_POS),                               // far-left
+		(Y_POS),                               // center
+		(Y_POS),                               // far-right
+		(Y_POS - (CANVAS_BG[0].height) / 2),   // bottom-left
+		(Y_POS - (CANVAS_BG[0].height) / 2),   // bottom-right
+		(Y_POS + CANVAS_BG[0].height)          // far-bottom
+	];
+	const num_off = 9;
+
+	for(let i = 0; i < num_off; i += 1) {
+		ctx.drawImage(
+			CANVAS_BG[0],
+			x_off[i], y_off[i],                          // src x, y
+			canvas.width, canvas.height,           // src w, h
+			0, 0, canvas.width, canvas.height      // dst x, y, w, h
+		);
+	}
+
+	// print some debug info
+	ctx.font = '14px monospace';
+	ctx.fillStyle = 'white';
+	ctx.fillText('X:         ' + X_POS, 10, 20);
+	ctx.fillText('X % width: ' + x_pos, 10, 40);
+	
+	// print fps too
+	let fps = Math.round(1/((timestamp - TIME_LAST) / 1000));
+	ctx.fillText("FPS: " + fps, 440, 20);
+
+	TIME_LAST = timestamp;
 	window.requestAnimationFrame(Render_Step);
 }
 
 function Render_Init()
 {
 	TIME_START = performance.now();
+	TIME_LAST = TIME_START;
 
-	// bg1 canvas
-	const canvas_bg1 = document.getElementById('bg1');
-	if (!canvas_bg1.getContext) { return; }
-	const ctx_bg1 = canvas_bg1.getContext('2d');
-	// draw background layer
-	Render_Step_BG1(ctx_bg1, 768, 768);
+	// get BG canvas
+	CANVAS_BG[0] = document.getElementById('bg0');
+	CTX_BG[0] = CANVAS_BG[0].getContext('2d');
+	// draw background layers
+	Render_Step_BG(CTX_BG[0], CANVAS_BG[0].width, CANVAS_BG[0].height);
 
 	// on each frame
 	window.requestAnimationFrame(Render_Step);
